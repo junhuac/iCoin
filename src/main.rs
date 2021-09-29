00c24bd5d3022a8e4d9efccd18513f6e80ace3e2
Junhua Chang <junhuac@hotmail.com>
2021-09-29 16:08:18 -0700

    Print iCoin Blockchain Platform using ferris_says

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("iCoin Blockchain Platform");
+    let stdout = stdout();
+    let message = String::from("iCoin Blockchain Platform");
+    let width = message.chars().count();
+
+
+    let mut Writer = BufWriter::new(stdout.lock());
+
+    say(message.as_bytes(), width, &mut Writer).unwrap();
 }
